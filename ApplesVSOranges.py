8ae2f87bb34b1783e3b0f28bbd3a42940cc05cde
Saveary <savearyj@gmail.com>
2017-07-06 14:21:31 -0400

    Additional Clarification of features

--- ApplesVSOranges.py
+++ ApplesVSOranges.py
@@ -1,6 +1,6 @@
 from sklearn import tree
 
-# features are the data 
+# features are the data - [weight, texture] - 0 = Bumpy / 1 = Smooth
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 
 # labels identifies if it is an apple (0) or orange (1)
