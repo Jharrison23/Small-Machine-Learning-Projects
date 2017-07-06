b77185b0a63e6457980719efc9a737f0b719857f
Saveary <savearyj@gmail.com>
2017-07-05 20:26:21 -0400

    initial commit for small file that tells the difference between apples vs oranges using characterstics of each

---
+++ ApplesVSOranges.py
@@ -0,0 +1,17 @@
+from sklearn import tree
+
+# features are the data 
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+
+# labels identifies if it is an apple (0) or orange (1)
+labels = [0, 0, 1, 1]
+
+# classifier - clf.fit = training algorithm. 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+# test case 
+print(clf.predict([[160, 0]]))
+
+
+
